c7df33b542b57c2e4123f96a6b8c28fc69f2a6b2
kaylarwright <kayla.wright@gompersstudent.org>
2019-05-13 14:27:53 -0700

    Add files via upload

--- GameObject.java
+++ GameObject.java
@@ -1,4 +1,22 @@
+import java.awt.Graphics;
 
 public class GameObject {
+int x;
+int y;
+int width;
+int height;
 
+GameObject(int x,int y, int width, int height){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+}
+void update() {
+	y++;
+	x++;
+}
+void draw(Graphics g) {
+	g.fillRect(x, y, width, height);
+}
 }
--- GamePanel.java
+++ GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,18 +11,45 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
 Timer time;
+final int MENU_STATE = 0;
+final int GAME_STATE = 1;
+final int END_STATE = 2;
+int currentState = MENU_STATE;
+Font titleFont;
+Font titleF;
+Font tFont;
+
+
 @Override
 public void paintComponent(Graphics g) {
-	g.fillRect(10, 10, 100, 100);
+	if(currentState == MENU_STATE) {
+		drawMenuState(g);
+	}else if(currentState == GAME_STATE) {
+		drawGameState(g);
+	}else if (currentState == END_STATE) {
+		drawEndState(g);
+	}
 }
 
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
+	
 	repaint();
+	if(currentState == MENU_STATE) {
+		updateMenuState();
+	}else if(currentState == GAME_STATE) {
+		updateGameState();
+	}else if(currentState == END_STATE) {
+		updateEndState();
+	}
 }
 GamePanel() {
-	time = new Timer(1000/6, this); 
+	time = new Timer(1000/6, this);
+	titleFont = new Font("Arial", Font.ITALIC, 48);
+	titleF = new Font("Arial", Font.ITALIC, 28);
+	tFont = new Font("Arial", Font.ITALIC, 48);
+
 }
 void startGame() {
 	time.start();
@@ -29,7 +58,12 @@ void startGame() {
 @Override
 public void keyPressed(KeyEvent arg0) {
 	// TODO Auto-generated method stub
-	System.out.println("a");
+	if(arg0.getKeyCode()== KeyEvent.VK_ENTER) {
+		currentState++;
+		if(currentState > END_STATE) {
+			currentState = MENU_STATE;
+		}
+	}
 }
 
 @Override
@@ -42,5 +76,35 @@ public void keyReleased(KeyEvent arg0) {
 public void keyTyped(KeyEvent arg0) {
 	// TODO Auto-generated method stub
 	System.out.println("to console");
+}
+void updateMenuState() {
+	
+}
+void updateGameState() {
+	
+}
+void updateEndState() {
+	
+}
+void drawMenuState(Graphics g) {
+	g.setColor(Color.GRAY);
+	g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+	g.setColor(Color.RED);
+	g.setFont(titleFont);
+	g.drawString("Mouse Maze", 120, 350);
+	g.setColor(Color.RED);
+	g.setFont(titleF);
+	g.drawString("By:Kayla Wright",150, 450);
+}
+void drawGameState(Graphics g) {
+	g.setColor(Color.GREEN);
+	g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+}
+void drawEndState (Graphics g) {
+	g.setColor(Color.BLUE);
+	g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
+	g.setColor(Color.RED);
+	g.setFont(tFont);
+	g.drawString("GAME OVER", 120, 400);
 }
 }
